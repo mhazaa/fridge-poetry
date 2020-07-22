b403145d6e7ac632d22e9bc404a8f37a323fcbed
Magdi Hazaa <magdihazaa@gmail.com>
2020-07-21 22:42:40 -0500

    set up client side main methods and add a test element in entries

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -2,13 +2,74 @@ import React from 'react';
 import './App.css';
 import socket from './socket.js';
 
+class Entry {
+  constructor(id, text, x, y){
+    this.id = id;
+    this.text = text;
+    this.x = x || 0;
+    this.y = y || 0;
+  }
+
+  move(x, y){
+    this.x = x;
+    this.y = y;
+    socket.emit('moveEntry', {
+      id: this.id,
+      x: this.x,
+      y: this.y
+    })
+  }
+}
+
 class App extends React.Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      entries: {}
+    }
   }
+
+  serverEvents(){
+    var that = this;
+
+    socket.emit('fetchAllEntries');
+
+    socket.on('fetchAllEntries', function(data){
+      that.setState({entries: data});
+    });
+
+    socket.on('newEntry', function(data){
+
+    });
+
+    socket.on('moveEntry', function(data){
+      //that.setState(...this.state.entries, {
+      //  data.id:
+      //});
+    });
+  }
+
+  componentDidMount(){
+    this.serverEvents();
+  }
+
   render(){
+    var that = this;
     return (
-      <div></div>
+      <div>
+
+        {Object.keys(this.state.entries).map(function(key){
+          var entry = that.state.entries[key];
+          var translate = 'translate(' + entry.x + 'px, ' + entry.y + 'px)';
+          return (
+            <div key={entry.id} id={entry.id} style={{ transform: translate }}>
+              <p>{entry.text}</p>
+            </div>
+          )
+        })}
+
+      </div>
     )
   }
 }
--- socketControls.js
+++ socketControls.js
@@ -1,23 +1,29 @@
 var Entry = require('./classes/Entry.js');
 var generateID = require('./functions/generateID');
-var entries = {};
+var entries = {
+  4324: new Entry(4324, 'yoyoyo', 100, 100)
+};
 
 var socketControls = function(io){
   io.on('connection', function(socket){
   	console.log(socket.id);
 
+    socket.on('fetchAllEntries', function(){
+      socket.emit('fetchAllEntries', entries);
+    });
+
     socket.on('newEntry', function(data){
       let id = generateID(entries); //will loop through the id proerty of each entry
       let entry = new Entry(id, data.text, data.x, data.y);
       entries[id] = entry;
-      io.sockets.emit(entry);
+      io.sockets.emit('newEntry', entry);
     });
 
     socket.on('moveEntry', function(data){
       var entry = entries[data.id];
       entry.x = data.x;
       entry.y = data.y;
-      io.sockets.emit(entry);
+      io.sockets.emit('moveEntry', entry);
     });
   });
 }
